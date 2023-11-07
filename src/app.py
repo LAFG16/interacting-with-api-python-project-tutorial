238f11aa33ad2f28c74057ea562357582aab5de2
LUIS ALEXIS DE FREITAS <131389113+LAFG16@users.noreply.github.com>
2023-11-07 20:07:25 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -2,14 +2,50 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import matplotlib.pyplot as plt
 
 # load the .env file variables
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+# making connection with the API
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+auth_manager = SpotifyClientCredentials(client_id= client_id, client_secret= client_secret)
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+# making API requests
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+artisturl = '4gzpq5DPGxSnKTe4SA8HAU'
+sp = spotipy.Spotify(auth_manager=auth_manager)
+results = sp.artist_top_tracks(artisturl)
+
+# extract name, popularity and duration in seconds
+
+tracks = []
+popularity = []
+duration = []
+for track in results['tracks'][:10]:
+    tracks.append(track['name'])
+    popularity.append(track['popularity'])
+    duration.append(round(track['duration_ms']/1000/60, 2))
+
+# transform request into DataFrame
+songs = {"Track": tracks, "Popularity": popularity, "Duration": duration}
+songs_df = pd.DataFrame(songs)
+
+#Top 3 songs
+
+songs_top_3 = songs_df[:3]
+songs_top_3
+
+# Analyzing statistical relationship with a plot
+
+x = songs_df['Duration']
+y = songs_df['Popularity']
+
+plt.scatter(x, y)
+plt.xlabel("DURATION")
+plt.ylabel("POPULARITY")
+plt.show()
